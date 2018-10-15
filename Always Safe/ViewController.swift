ca519f7839819cef8688cb31c029871a4764e41a
Mirkoricci94 <ricci.mirko94@gmail.com>
2018-10-15 12:35:46 +0200

    o

--- Always Safe/ViewController.swift
+++ Always Safe/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
        
         var greek : String
+         var ciao : String
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
