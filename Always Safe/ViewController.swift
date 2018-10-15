173a66ef75ed60f3117b12e4e97566c0d2a53e5e
didimimimi <jimx4ever@yahoo.com>
2018-10-15 12:38:38 +0200

    dim

--- Always Safe/ViewController.swift
+++ Always Safe/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         var kalispera : String
         var greek : String
          var ciao : String
+        var buenosdias : String
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
